1123733c86d6b12237a57aef7d90f39e0f3907ba
Noe Thalheim <noe@thalheim.email>
2022-06-16 00:25:08 +0200

    fix: correct layout path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: ["./{src,tutorial}/**/*.{html,jl}"],
+content: ["./{_layout,tutorial}/**/*.{html,jl}"],
   theme: {
     extend: {},
   },
